d3b019b890cbe53e7af0e7ec2e1eab6e69217184
Kamil <sd@wp.pl>
2023-10-04 15:41:21 +0200

    1

--- 9_pandas.py
+++ 9_pandas.py
@@ -8,5 +8,19 @@ print(df.head())
 print(df['class'].value_counts())
 
 species = {
-    "Iris-setosa":0, "Iris-versicolor":1, "Iris-virginica":2'
+    "Iris-setosa":0, "Iris-versicolor":1, "Iris-virginica":2
 }
+
+df['class_value'] = df['class'].map(species)
+print(df.head())
+
+# sns.scatterplot(data=df, x='sepallength', y='sepalwidth', hue='class')
+# plt.title('Sepal')
+# plt.show()
+# sns.scatterplot(data=df, x='petallength', y='petalwidth', hue='class')
+# plt.title('Petal')
+# plt.show()
+
+print(df.columns)
+sns.heatmap(df.iloc[:,:4].corr(), annot=True)
+plt.show()
