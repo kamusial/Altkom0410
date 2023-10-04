4273f1c75dc8728c23b3ae43200f2fb9a38a14d2
Kamil <sd@wp.pl>
2023-10-04 10:55:54 +0200

    1

--- 0_przyklady.py
+++ 0_przyklady.py
@@ -14,6 +14,21 @@ zmienna3 = '5'
 moja_lista1 = []
 moja_lista2 = [4, 2.2, 'Kamil', 'piesek', 343]
 print(type(moja_lista2))
-print(moja_lista2[2])
-print(moja_lista2[2:4])
+print(moja_lista2[2])    #wyświetlanie elementu listy
+print(moja_lista2[2:4])    #slicing
 
+moja_lista2.append(6)   #dodawanie do listy
+print(moja_lista2)     #wyświetlanie listy
+
+dane1 = []
+for i in range(6, 19, 2):   #od, do, krok
+    dane1.append(i+1)
+print(dane1)
+
+dane2 = []
+dane2 = [   i+1    for i in range(4, 55, 3)  ]
+print(dane2)
+
+dane3 = []
+dane3 = [i*i if i % 2 == 0 else 0 for i in range(5, 54, 3)]
+print(dane3)
--- 1_pyplot.py
+++ 1_pyplot.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
+#przyklad1
 numbers = [5, 10, 15, 3, 20]
-
 # plt.plot(numbers, 'o') # niepołączone punkty
 # plt.plot(numbers, '.') # niepołączone kropki
 # plt.plot(numbers, 's') # niepołączone kwadraty
@@ -12,3 +12,28 @@ plt.plot(numbers, 'k*') # czarne gwiazdki
 # plt.plot(numbers, 'bD:') # niebieskie diamenty połączone kropkami
 # plt.plot(numbers, 'mp--') # różowe pięciokąty połączone przerywanymi liniam
 plt.show()
+
+# przyklad 2
+# funkcja y = 5x - 2
+X = [i+1 for i in range(10)]
+Y = [5*i - 2 for i in X]
+plt.plot(X, Y, 'ro-')
+plt.show()
+
+# przykład 3
+# funkcja 1: y = 5x - 2 ; funkcja 2: y = -2x + 5 ; funkcja 3: y = 3x + 3
+X = [i+1 for i in range(-10, 10)]
+Y1 = [5*i - 2 for i in X]
+Y2 = [-2*i + 5 for i in X]
+Y3 = [3*i + 3 for i in X]
+plt.axhline()  # linia pozioma osi
+plt.axvline()  # linia pionowa osi
+plt.plot(X, Y1, 'ro-')
+plt.plot(X, Y2, 'b^-')
+plt.plot(X, Y3, 'gs-')
+plt.xlabel('punkty x')   #opis osi X
+plt.ylabel('punkty y')   #opis osi Y
+plt.title('Wykres funkcji')
+plt.show()
+
+
