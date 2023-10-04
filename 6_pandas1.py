78c7051a6debfe7ae005871c22889bf88c3ce030
Kamil <sd@wp.pl>
2023-10-04 12:50:26 +0200

    1

--- 6_pandas1.py
+++ 6_pandas1.py
@@ -4,4 +4,23 @@ import seaborn as sns
 from sklearn.linear_model import LinearRegression   # scikit-learn
 
 df = pd.read_csv('weight-height.csv')
-print(df)
+print(type(df))
+print(df.head(8))
+print(df.Height)   #dana kolumna
+print(df.Gender.value_counts())
+df.Height *= 2.54
+df.Weight /= 2.2
+print(df.head(8))
+# plt.hist(df.Weight, 40)
+# plt.show()
+
+# plt.hist(df.query("Gender=='Male'").Weight, 40, color='red')
+# plt.hist(df.query("Gender=='Female'").Weight, 40)
+# plt.show()
+
+# sns.histplot(df.query("Gender=='Male'").Weight)
+# sns.histplot(df.query("Gender=='Female'").Weight)
+# plt.show()
+
+sns.histplot(df, x=df.Weight, hue='Gender', element='poly')  #bars, step
+plt.show()
